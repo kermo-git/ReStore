1115bb463d0695af39425996a4e6e962bd25c976
kermo-git <saarse27@gmail.com>
2022-04-20 17:05:35 +0300

    Basket page table styling (Episode 72)

--- client/src/Features/Basket/BasketPage.tsx
+++ client/src/Features/Basket/BasketPage.tsx
@@ -1,8 +1,11 @@
-import { Typography } from "@mui/material";
+import { Delete } from "@mui/icons-material";
+import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import Agent from "../../App/API/Agent";
 import Loading from "../../App/Layout/Loading";
 import { Basket } from "../../App/Models/Basket";
+
+const formatPrice = (price: number) => (price / 100).toFixed(2) + "€"
 
 export default function BasketPage() {
 	const [loading, setLoading] = useState<boolean>(true)
@@ -17,6 +20,40 @@ export default function BasketPage() {
 
 	if (loading) return <Loading message="Loading basket ..."/>
 	if (!basket) return <Typography variant="h3">You don't have any items in your basket</Typography>
-	
-	return <Typography variant="h3">BuyerId = {basket.buyerId}</Typography>
+
+	return (
+		<TableContainer component={Paper}>
+			<Table sx={{ minWidth: 650 }} aria-label="simple table">
+				<TableHead>
+					<TableRow>
+						<TableCell>Prduct</TableCell>
+						<TableCell align="right">Price</TableCell>
+						<TableCell align="right">Quantity</TableCell>
+						<TableCell align="right">Subtotal</TableCell>
+						<TableCell align="right"></TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{basket.items.map((item) => (
+						<TableRow
+							key={item.productId}
+							sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+							>
+							<TableCell component="th" scope="row">
+							{item.name}
+							</TableCell>
+							<TableCell align="right">{formatPrice(item.price)}</TableCell>
+							<TableCell align="right">{item.quantity}</TableCell>
+							<TableCell align="right">{formatPrice(item.price * item.quantity)}</TableCell>
+							<TableCell align="right">
+								<IconButton color="error">
+									<Delete/>
+								</IconButton>
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
+	);
 }
