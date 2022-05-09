757f58e7ecfedccc5448af0e28549126fcbeb845
kermo-git <saarse27@gmail.com>
2022-05-09 13:29:22 +0300

    Display basket quantity on product details page (Episode 80)

--- client/src/Features/Catalog/ProductDetails.tsx
+++ client/src/Features/Catalog/ProductDetails.tsx
@@ -1,30 +1,39 @@
-import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, TextField, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+
 import Agent from "../../App/API/Agent";
+import { useStoreContext } from "../../App/Context/StoreContext";
 import NotFound from "../../App/Errors/NotFound";
 import Loading from "../../App/Layout/Loading";
 import { Product } from "../../App/Models/Product";
 import { formatPrice } from "../../App/Utils";
 
 export default function ProductDetailsPage() {
+	const {basket} = useStoreContext()
     const {id} = useParams<{id: string}>()
+
     const [product, setProduct] = useState<Product | null>(null)
-    const [loading, setLoading] = useState<boolean>(true)
+	const [basketQuantity, setBasketQuantity] = useState(0)
+
+    const [productLoading, setProductLoading] = useState(true)
+	const [submitting, setSubmitting] = useState(false)
+
+	const item = basket?.items.find(item => item.productId === product?.id)
 
     useEffect(() => {
+		if (item) setBasketQuantity(item.quantity)
+		
         Agent.Catalog.details(id || "")
         .then(product => setProduct(product))
         .catch(error => console.error(error))
-        .finally(() => setLoading(false))
-    }, [id])
+        .finally(() => setProductLoading(false))
+    }, [id, item])
 
-    if (loading) return <Loading message="Loading product ..."/>
+    if (productLoading) return <Loading message="Loading product ..."/>
 
-    if (!product)
-        return (
-            <NotFound/>
-        )
+    if (!product) return <NotFound/>
 
     return (
         <Grid container spacing={6}>
@@ -59,6 +68,28 @@ export default function ProductDetailsPage() {
                         </TableBody>
                     </Table>
                 </TableContainer>
+				<Grid container spacing={2}>
+					<Grid item xs={6}>
+						<TextField
+							variant="outlined"
+							type="number"
+							label="Quantity in cart"
+							fullWidth
+							value={basketQuantity}
+						/>
+					</Grid>
+					<Grid item xs={6}>
+						<LoadingButton 
+							sx={{height: "55px"}}
+							color="primary"
+							size="large"
+							variant="contained"
+							fullWidth
+						>
+							{item ? "Update Quantity" : "Add to cart"}
+						</LoadingButton>
+					</Grid>
+				</Grid>
             </Grid>
         </Grid>
     )
