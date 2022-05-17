01a11efece56475d8ddc78d96e3ac553f8b41e99
kermo-git <saarse27@gmail.com>
2022-05-17 14:41:46 +0300

    Redux actions (Episode 86)

--- client/src/Features/Contact/ContactPage.tsx
+++ client/src/Features/Contact/ContactPage.tsx
@@ -1,9 +1,10 @@
-import { Typography } from "@mui/material";
-import { useSelector } from "react-redux";
+import { Button, ButtonGroup, Typography } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
 
-import { CounterState } from "./CounterReducer";
+import { CounterState, DECREMENT_COUNTER, INCREMENT_COUNTER } from "./CounterReducer";
 
 export default function ContactPage() {
+	const dispatch = useDispatch()
 	const {data, title} = useSelector((state: CounterState) => state)
 	
     return (<>
@@ -13,5 +14,19 @@ export default function ContactPage() {
 		<Typography variant="h5">
             Data: {data}
         </Typography>
+		<ButtonGroup>
+			<Button 
+				onClick={ev => {dispatch({type: DECREMENT_COUNTER})}}
+				variant="contained" 
+				color="error">
+				Decrement
+			</Button>
+			<Button 
+				onClick={ev => {dispatch({type: INCREMENT_COUNTER})}}
+				variant="contained" 
+				color="primary">
+				Increment
+			</Button>
+		</ButtonGroup>
 	</>)
 }
--- client/src/Features/Contact/CounterReducer.ts
+++ client/src/Features/Contact/CounterReducer.ts
@@ -1,4 +1,7 @@
 
+export const INCREMENT_COUNTER = "INCREMENT_COUNTER"
+export const DECREMENT_COUNTER = "DECREMENT_COUNTER"
+
 export interface CounterState {
 	data: number,
 	title: string
@@ -10,5 +13,12 @@ const initialState: CounterState = {
 }
 
 export default function counterReducer(state = initialState, action: any) {
-	return initialState
+	switch (action.type) {
+		case INCREMENT_COUNTER:
+			return {...state, data: state.data + 1}
+		case DECREMENT_COUNTER:
+			return {...state, data: state.data - 1}
+		default:
+			return state
+	}
 }
