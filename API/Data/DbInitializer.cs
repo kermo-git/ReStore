b3edd5133c6e0c9fbca6e8913a596113857f37ae
kermo-git <saarse27@gmail.com>
2022-05-20 17:08:33 +0300

    User and roles database setup and migration (Episode 124)

--- API/Data/DbInitializer.cs
+++ API/Data/DbInitializer.cs
@@ -1,10 +1,27 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
 
 namespace API.Data {
     public static class DbInitializer {
-        public static void Initialize(StoreContext context) {
+        public static async Task Initialize(StoreContext context, UserManager<User> userManager) {
+			if (!userManager.Users.Any()) {
+				var bob = new User {
+					UserName = "bob",
+					Email = "bob@dotnet.com"
+				};
+				await userManager.CreateAsync(bob, "Pa$$w0rd");
+				await userManager.AddToRoleAsync(bob, "Member");
+
+				var admin = new User {
+					UserName = "admin",
+					Email = "admin@dotnet.com"
+				};
+				await userManager.CreateAsync(admin, "Pa$$w0rd");
+				await userManager.AddToRolesAsync(admin, new[]{"Admin", "Member"});
+			}
             if (context.Products.Any()) return;
 
             var products = new List<Product> {
