f7a7da06908bd9e7a01f033a1cd38e070df30cee
kermo-git <saarse27@gmail.com>
2022-05-30 16:27:21 +0300

    Configuring swagger to send authentification token (Episode 131)

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using System.Collections.Generic;
 
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
@@ -30,6 +31,25 @@ namespace API {
             services.AddControllers();
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+				c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme {
+					Description = "Jwt auth header",
+					Name = "Authorization",
+					In = ParameterLocation.Header,
+					Type = SecuritySchemeType.ApiKey,
+					Scheme = "Bearer"
+				});
+				c.AddSecurityRequirement(new OpenApiSecurityRequirement {{
+					new OpenApiSecurityScheme {
+						Reference = new OpenApiReference {
+							Type = ReferenceType.SecurityScheme,
+							Id = "Bearer"
+						},
+						Scheme = "oauth2",
+						Name = "Bearer",
+						In = ParameterLocation.Header
+					},
+					new List<string>()
+				}});
             });
             services.AddDbContext<StoreContext>(opt => {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
