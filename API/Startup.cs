520fe575c1ca9a09996c80e042a27889af5c6f8f
kermo-git <saarse27@gmail.com>
2022-06-16 17:04:03 +0300

    Serving client application from API server (Episode 186)

--- API/Startup.cs
+++ API/Startup.cs
@@ -89,6 +89,9 @@ namespace API {
 
             app.UseRouting();
 
+			app.UseDefaultFiles();
+			app.UseStaticFiles();
+
             app.UseCors(opt =>
                 opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000")
             );
@@ -97,6 +100,7 @@ namespace API {
 
             app.UseEndpoints(endpoints => {
                 endpoints.MapControllers();
+				endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
