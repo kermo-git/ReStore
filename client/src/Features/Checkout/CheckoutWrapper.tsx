cf2a253c697995f8810e6d9df2cf4f6dafe19d63
kermo-git <saarse27@gmail.com>
2022-06-16 12:27:35 +0300

    Fixed a small error (Episode 177)

--- client/src/Features/Checkout/CheckoutWrapper.tsx
+++ client/src/Features/Checkout/CheckoutWrapper.tsx
@@ -7,6 +7,7 @@ import CheckoutPage from "./CheckoutPage";
 import { useAppDispatch } from "../../App/Store/ConfigureStore";
 import Agent from "../../App/API/Agent";
 import Loading from "../../App/Layout/Loading";
+import { setBasket } from "../Basket/BasketSlice";
 
 const stripePromise = loadStripe("pk_test_51LAsvKHNKzT2V29Ne2zXEFzHQ11tvn4wzWNCCa33iDgmRhExH9vyouLBU79utGFXMwKZdVBHaX9z4Zb26H8lEJFb00dNpVD8rs");
 
@@ -16,7 +17,7 @@ export default function CheckoutWrapper() {
 
 	useEffect(() => {
 		Agent.Payment.createPaymentIntent()
-			.then(basket => dispatch(basket))
+			.then(basket => dispatch(setBasket(basket)))
 			.catch(error => console.log(error))
 			.finally(() => setLoading(false))
 	}, [dispatch])
