3fc2184f9e275dd1312aec1e7d972fedda23e04c
kermo-git <saarse27@gmail.com>
2022-06-16 10:15:53 +0300

    Creating a payment intent on checkout page (Episode 177)

--- client/src/App/API/Agent.ts
+++ client/src/App/API/Agent.ts
@@ -58,8 +58,8 @@ const responseBody = (response: AxiosResponse) => response.data
 
 const requests = {
     get: (url: string, params?: URLSearchParams) => axios.get(url, {params}).then(responseBody),
-    post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
-    put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
+    post: (url: string, body?: {}) => axios.post(url, body).then(responseBody),
+    put: (url: string, body?: {}) => axios.put(url, body).then(responseBody),
     delete: (url: string) => axios.delete(url).then(responseBody)  
 }
 
@@ -71,7 +71,7 @@ const Catalog = {
 
 const Basket = {
 	get: () => requests.get("basket"),
-	addItem: (productId: number, quantity: number = 1) => requests.post(`basket?productId=${productId}&quantity=${quantity}`, {}),
+	addItem: (productId: number, quantity: number = 1) => requests.post(`basket?productId=${productId}&quantity=${quantity}`),
 	removeItem: (productId: number, quantity: number = 1) => requests.delete(`basket?productId=${productId}&quantity=${quantity}`)
 }
 
@@ -96,11 +96,16 @@ const Orders = {
 	create: (values: any) => requests.post("orders", values)
 }
 
+const Payment = {
+	createPaymentIntent: () => requests.post("payment")
+}
+
 const Agent = {
 	Catalog,
 	Basket,	
     TestErrors,
 	Account,
-	Orders
+	Orders,
+	Payment
 }
 export default Agent
--- client/src/Features/Checkout/CheckoutWrapper.tsx
+++ client/src/Features/Checkout/CheckoutWrapper.tsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react";
+
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
 import CheckoutPage from "./CheckoutPage";
+import { useAppDispatch } from "../../App/Store/ConfigureStore";
+import Agent from "../../App/API/Agent";
+import Loading from "../../App/Layout/Loading";
 
 const stripePromise = loadStripe("pk_test_51LAsvKHNKzT2V29Ne2zXEFzHQ11tvn4wzWNCCa33iDgmRhExH9vyouLBU79utGFXMwKZdVBHaX9z4Zb26H8lEJFb00dNpVD8rs");
 
 export default function CheckoutWrapper() {
+	const dispatch = useAppDispatch()
+	const [loading, setLoading] = useState(true)
+
+	useEffect(() => {
+		Agent.Payment.createPaymentIntent()
+			.then(basket => dispatch(basket))
+			.catch(error => console.log(error))
+			.finally(() => setLoading(false))
+	}, [dispatch])
+
+	if (loading) return <Loading message="Loading checkout..."/>
+
 	return (
 		<Elements stripe={stripePromise}>
 			<CheckoutPage/>	
