5ed955b3398b14db927b70fb2a41b7e2f25df439
kermo-git <saarse27@gmail.com>
2022-06-16 18:00:05 +0300

    Switched to PostgreSQL (Episode 187)

--- API/Startup.cs
+++ API/Startup.cs
@@ -51,7 +51,7 @@ namespace API {
 				}});
             });
             services.AddDbContext<StoreContext>(opt => {
-                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+                opt.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
             });
             services.AddCors();
 
