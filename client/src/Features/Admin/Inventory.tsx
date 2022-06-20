914c4735a8481f5d817a4702d9b4a329aaa02dff
kermo-git <saarse27@gmail.com>
2022-06-20 14:36:55 +0300

    Formatting changes

--- client/src/Features/Admin/Inventory.tsx
+++ client/src/Features/Admin/Inventory.tsx
@@ -15,61 +15,59 @@ export default function Inventory() {
 		dispatch(setMetaData({...metaData, currentPage: page}))
 	}
 
-    return (
-        <>
-            <Box display='flex' justifyContent='space-between'>
-                <Typography sx={{ p: 2 }} variant='h4'>Inventory</Typography>
-                <Button sx={{ m: 2 }} size='large' variant='contained'>Create</Button>
-            </Box>
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>#</TableCell>
-                            <TableCell align="left">Product</TableCell>
-                            <TableCell align="right">Price</TableCell>
-                            <TableCell align="center">Type</TableCell>
-                            <TableCell align="center">Brand</TableCell>
-                            <TableCell align="center">Quantity</TableCell>
-                            <TableCell align="right"></TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {products.map((product) => (
-                            <TableRow
-                                key={product.id}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell component="th" scope="row">
-                                    {product.id}
-                                </TableCell>
-                                <TableCell align="left">
-                                    <Box display='flex' alignItems='center'>
-                                        <img src={product.pictureURL} alt={product.name} style={{ height: 50, marginRight: 20 }} />
-                                        <span>{product.name}</span>
-                                    </Box>
-                                </TableCell>
-                                <TableCell align="right">{formatPrice(product.price)}</TableCell>
-                                <TableCell align="center">{product.type}</TableCell>
-                                <TableCell align="center">{product.brand}</TableCell>
-                                <TableCell align="center">{product.quantityInStock}</TableCell>
-                                <TableCell align="right">
-                                    <Button startIcon={<Edit />} />
-                                    <Button startIcon={<Delete />} color='error' />
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-			{metaData && (
-				<Box sx={{pt: 2, pb: 2}}>
-					<AppPagination
-						metaData={metaData}
-						onPageChange={onPageChange}
-					/>
-				</Box>
-			)}
-        </>
-    )
+	return (<>
+		<Box display='flex' justifyContent='space-between'>
+			<Typography sx={{ p: 2 }} variant='h4'>Inventory</Typography>
+			<Button sx={{ m: 2 }} size='large' variant='contained'>Create</Button>
+		</Box>
+		<TableContainer component={Paper}>
+			<Table sx={{ minWidth: 650 }} aria-label="simple table">
+				<TableHead>
+					<TableRow>
+						<TableCell>#</TableCell>
+						<TableCell align="left">Product</TableCell>
+						<TableCell align="right">Price</TableCell>
+						<TableCell align="center">Type</TableCell>
+						<TableCell align="center">Brand</TableCell>
+						<TableCell align="center">Quantity</TableCell>
+						<TableCell align="right"></TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{products.map((product) => (
+						<TableRow
+						key={product.id}
+						sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+						>
+							<TableCell component="th" scope="row">
+								{product.id}
+							</TableCell>
+							<TableCell align="left">
+								<Box display='flex' alignItems='center'>
+									<img src={product.pictureURL} alt={product.name} style={{ height: 50, marginRight: 20 }} />
+									<span>{product.name}</span>
+								</Box>
+							</TableCell>
+							<TableCell align="right">{formatPrice(product.price)}</TableCell>
+							<TableCell align="center">{product.type}</TableCell>
+							<TableCell align="center">{product.brand}</TableCell>
+							<TableCell align="center">{product.quantityInStock}</TableCell>
+							<TableCell align="right">
+								<Button startIcon={<Edit />} />
+								<Button startIcon={<Delete />} color='error' />
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
+		{metaData && (
+			<Box sx={{pt: 2, pb: 2}}>
+				<AppPagination
+					metaData={metaData}
+					onPageChange={onPageChange}
+				/>
+			</Box>
+		)}
+	</>)
 }
