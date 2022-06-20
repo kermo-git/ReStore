e9f37a372a2cdca485bf5be4777f18c81179e6ef
kermo-git <saarse27@gmail.com>
2022-06-20 15:56:44 +0300

    Reusable select list (Episode 203)

--- client/src/Features/Admin/ProductForm.tsx
+++ client/src/Features/Admin/ProductForm.tsx
@@ -1,10 +1,12 @@
+import { useEffect } from "react"
 import { useForm } from "react-hook-form"
 
 import { Typography, Grid, Paper, Box, Button } from "@mui/material"
 
 import AppTextInput from "../../App/components/AppTextInput"
 import { Product } from "../../App/Models/Product"
-import { useEffect } from "react"
+import { useProducts } from "../../App/Hooks/UseProducts"
+import AppSelectList from "../../App/components/AppSelectList"
 
 interface Props {
 	product?: Product
@@ -13,6 +15,7 @@ interface Props {
 
 export default function ProductForm({product, cancelEdit}: Props) {
 	const { control, reset } = useForm()
+	const { brands, types } = useProducts()
 
 	useEffect(() => {
 		if (product) reset(product)
@@ -28,10 +31,10 @@ export default function ProductForm({product, cancelEdit}: Props) {
 					<AppTextInput control={control} name='name' label='Product name' />
 				</Grid>
 				<Grid item xs={12} sm={6}>
-					<AppTextInput control={control} name='brand' label='Brand' />
+					<AppSelectList control={control} name='brand' label='Brand' items={brands}/>
 				</Grid>
 				<Grid item xs={12} sm={6}>
-					<AppTextInput control={control} name='type' label='Type' />
+					<AppSelectList control={control} name='type' label='Type' items={types}/>
 				</Grid>
 				<Grid item xs={12} sm={6}>
 					<AppTextInput control={control} name='price' label='Price' />
